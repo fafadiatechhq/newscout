e5acc6122238e900a168698f8bf708e66c394a8b
Gauri Patil <gauri.patil@fafadiatech.com>
2026-02-17 11:07:18 +0530

    ref #17: added articles folder

---
+++ frontend/newscout/app/articles/[id]/page.tsx
@@ -0,0 +1,12 @@
+import ArticleDetail from "@/components/articles/ArticleDetail";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ArticleDetail />
+    </div>
+  );
+};
+
+export default page;
---
+++ frontend/newscout/components/articles/ArticleDetail.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Layout from "@/components/Layout";
+
+import React from "react";
+
+const ArticleDetail = () => {
+  return (
+    <div>
+      <Layout>
+        hello
+      </Layout>
+    </div>
+  );
+};
+
+export default ArticleDetail;
