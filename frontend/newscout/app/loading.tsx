2a3795091136a598759d0bdc590564103ba4c5f9
Teesha Vaza <teesha.vaza@fafadiatech.com>
2026-02-23 12:56:45 +0530

    ref #35 : The nextjs routing have been added and a suspense and loader have been added globally

---
+++ frontend/newscout/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-lg font-medium">Loading application...</p>
+    </div>
+  )
+}
+
+export default loading
