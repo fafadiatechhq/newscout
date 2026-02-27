a2948c51c6455e123cba76a19e435c497a067ccd
sarvesh.devrukhkar <sarvesh.devrukhkar@fafadiatech.com>
2026-02-27 15:20:44 +0530

    Ref #28 Implemented `/admin/team` page.

---
+++ frontend/newscout/app/admin/team/page.tsx
@@ -0,0 +1,14 @@
+import TeamContainer from "@/components/admin/team/TeamContainer";
+import AdminLayout from "@/components/admin/AdminLayout";
+
+const AdminTeamPage = () => {
+  return (
+    <>
+      <AdminLayout>
+        <TeamContainer />
+      </AdminLayout>
+    </>
+  );
+};
+
+export default AdminTeamPage;
