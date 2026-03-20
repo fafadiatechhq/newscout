f08498d4eb2345430d67267db6dab74bc4397783
Teesha Vaza <teesha.vaza@fafadiatech.com>
2026-03-20 10:27:50 +0530

    ref #80 : Made changes in api-docs page.tsx page

--- frontend/newscout/app/api-docs/page.tsx
+++ frontend/newscout/app/api-docs/page.tsx
@@ -1,14 +1,12 @@
-// app/api-docs-mdx/page.tsx
-import MDXClientWrapper from '@/components/api-docs-mdx/MDXClientWrapper'
+import MDXClientWrapper from '@/components/api-docs/MDXClientWrapper'
 
 export const metadata = {
   title: 'API Documentation',
   description: "Integrate NewScout's aggregated news into your applications.",
 }
 
-export default function Page() {
+export default function ApiDocsPage() {
   return (
       <MDXClientWrapper />
-
   )
 }
