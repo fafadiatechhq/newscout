96e0b8c0fb183ae40f40c692ab2b1ddc404a12f9
Teesha Vaza <teesha.vaza@fafadiatech.com>
2026-03-20 11:14:33 +0530

    ref #80 : Deleted Docusaurus files

--- frontend/newscout/components/api-docs-mdx/MDXClientWrapper.tsx
+++
@@ -1,8 +0,0 @@
-// components/api-docs-mdx/MDXClientWrapper.tsx
-'use client'
-
-import Content from './ApiDocsMdxContainer.mdx'
-
-export default function MDXClientWrapper() {
-  return <Content />
-}
