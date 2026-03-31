599c77bccf21d46dd8502e95dee4893032c1bacb
omkar_somaskar <omkar.somaskar@fafadiatech.com>
2026-03-31 14:38:50 +0530

    hotfix : fix the component routing issue

--- frontend/newscout/components/api-docs/MDXClientWrapper.tsx
+++ frontend/newscout/components/api-docs/MDXClientWrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Content from './ApiDocsMdxContainer.mdx'
+import Content from '../../components/api-docs/ApiDocsContainer.mdx'
 
 export default function MDXClientWrapper() {
   return <Content />
